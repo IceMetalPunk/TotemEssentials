dbf969e17f550c5ad0ff3cb211963ffc25c4980f
IceMetalPunk <icemetalpunk@gmail.com>
2017-09-02 15:56:42 -0400

    Updated TODO list

--- src/main/java/com/icemetalpunk/totemessentials/README.java
+++ src/main/java/com/icemetalpunk/totemessentials/README.java
@@ -3,10 +3,11 @@ package com.icemetalpunk.totemessentials;
 public class README {
 	/*
 	 * TODO:
-	 * 1. Add model/texture assets for phasing_totem, totem_shell, and essence_reaper.
-	 * 2. Add Totem of Reaping (read concept) + Recipe
-	 * 3. Continue on with concept stuff...
-	 * 4. Illusioners spawn in mansions:
+	 * 1. Add new totems and essence.
+	 * 2. Add totem stand.
+	 * 
+	 * Eventually, on its own branch:
+	 * ?. Illusioners spawn in mansions:
 	 * --> world.getChunkProvider().isInsideStructure(world, "Mansion", blockpos)
 	 */
 }
