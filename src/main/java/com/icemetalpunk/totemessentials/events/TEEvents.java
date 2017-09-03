f921ac91967b1932dd30899eb384dbd6bb087b47
IceMetalPunk <icemetalpunk@gmail.com>
2017-09-03 19:00:40 -0400

    Got Illusioner spawn code working, if hacky

--- src/main/java/com/icemetalpunk/totemessentials/events/TEEvents.java
+++ src/main/java/com/icemetalpunk/totemessentials/events/TEEvents.java
@@ -1,10 +1,16 @@
 package com.icemetalpunk.totemessentials.events;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 
 import com.icemetalpunk.totemessentials.TotemEssentials;
 
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.objects.ObjectIterator;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.EnumCreatureType;
@@ -22,19 +28,26 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome.SpawnListEntry;
 import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkGeneratorOverworld;
 import net.minecraft.world.gen.ChunkProviderServer;
+import net.minecraft.world.gen.IChunkGenerator;
+import net.minecraft.world.gen.structure.MapGenStructure;
+import net.minecraft.world.gen.structure.StructureComponent;
+import net.minecraft.world.gen.structure.StructureStart;
+import net.minecraft.world.gen.structure.WoodlandMansion;
 import net.minecraftforge.event.entity.living.LivingDropsEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent.PlayerTickEvent;
+import net.minecraftforge.fml.relauncher.ReflectionHelper;
 
 public class TEEvents {
 	private HashMap<Class<? extends Entity>, HashMap<Item, ItemStack>> dropReplacements = new HashMap<Class<? extends Entity>, HashMap<Item, ItemStack>>();
 	private HashMap<Class<? extends Entity>, Item> essenceMap = new HashMap<Class<? extends Entity>, Item>();
 
-	public final SpawnListEntry illusionerSpawn = new SpawnListEntry(EntityIllusionIllager.class, 500, 5, 5);
+	public final SpawnListEntry illusionerSpawn = new SpawnListEntry(EntityIllusionIllager.class, 100, 1, 1);
 
 	public TEEvents() {
 		// Populate drop replacements map
@@ -110,6 +123,67 @@ public class TEEvents {
 		}
 	}
 
+	// Really hacky re-building of Mansion isInsideStructure code that ignores
+	// the wonky isValid flag.
+	private boolean isInMansion(ChunkProviderServer prov, World world, BlockPos pos) {
+		IChunkGenerator chunkGen = prov.chunkGenerator;
+		if (chunkGen instanceof ChunkGeneratorOverworld) {
+			ChunkGeneratorOverworld overworldGen = (ChunkGeneratorOverworld) chunkGen;
+
+			// Woodland Mansion Generator is private; need to reflect into it?
+			Field mansionField = ReflectionHelper.findField(ChunkGeneratorOverworld.class, "woodlandMansionGenerator",
+					"field_191060_C", "C");
+			try {
+				WoodlandMansion mansionGen = (WoodlandMansion) mansionField.get(overworldGen);
+				Method initializeMethod = ReflectionHelper.findMethod(MapGenStructure.class, "initializeStructureData",
+						"func_143027_a", World.class);
+				initializeMethod.invoke(mansionGen, world);
+				return getMansionAtIgnoreFlag(mansionGen, pos) != null;
+
+			} catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+				e.printStackTrace();
+			}
+
+		}
+		return prov.chunkGenerator.isInsideStructure(world, "Mansion", pos); // Fallback
+	}
+
+	private StructureStart getMansionAtIgnoreFlag(MapGenStructure gen, BlockPos pos) {
+		Field mapField = ReflectionHelper.findField(MapGenStructure.class, "structureMap", "field_75053_d", "c");
+
+		try {
+			Long2ObjectMap<StructureStart> map = (Long2ObjectMap<StructureStart>) mapField.get(gen);
+
+			ObjectIterator objectiterator = map.values().iterator();
+			label31: while (objectiterator.hasNext()) {
+				StructureStart structurestart = (StructureStart) objectiterator.next();
+
+				// Removed sizeableStructure check from the below condition.
+				if (structurestart.getBoundingBox().isVecInside(pos)) {
+					Iterator<StructureComponent> iterator = structurestart.getComponents().iterator();
+
+					while (true) {
+						if (!iterator.hasNext()) {
+							continue label31;
+						}
+
+						StructureComponent structurecomponent = iterator.next();
+
+						if (structurecomponent.getBoundingBox().isVecInside(pos)) {
+							break;
+						}
+					}
+
+					return structurestart;
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+
 	// FIXME: Make Illusioners spawn in Mansions; DOESN'T WORK.
 	@SubscribeEvent
 	public void alterSpawns(WorldEvent.PotentialSpawns ev) {
@@ -120,7 +194,7 @@ public class TEEvents {
 		if (ev.getType() == EnumCreatureType.MONSTER && prov instanceof ChunkProviderServer) {
 			ChunkProviderServer serverProv = (ChunkProviderServer) prov;
 			// System.out.println("Is Monster & Server Provider");
-			if (serverProv.isInsideStructure(world, "Mansion", pos)) {
+			if (isInMansion(serverProv, world, pos)) {
 				// System.out.println("Mansion at " + pos);
 				ev.getList().clear();
 				ev.getList().add(illusionerSpawn);
