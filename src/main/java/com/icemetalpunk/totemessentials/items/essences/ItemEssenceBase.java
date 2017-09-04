6a0d465a6fe27d1e78aada95408be9a2c14475a5
IceMetalPunk <icemetalpunk@gmail.com>
2017-09-04 02:58:40 -0400

    Added Ore Dictionary entries for essences and totems

--- src/main/java/com/icemetalpunk/totemessentials/items/essences/ItemEssenceBase.java
+++ src/main/java/com/icemetalpunk/totemessentials/items/essences/ItemEssenceBase.java
@@ -1,11 +1,26 @@
 package com.icemetalpunk.totemessentials.items.essences;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 import com.icemetalpunk.totemessentials.items.TEItem;
+
+import net.minecraftforge.oredict.OreDictionary;
 
 public class ItemEssenceBase extends TEItem {
 
+	public final String essenceType;
+
 	public ItemEssenceBase(String name) {
 		super(name);
+		if (name.startsWith("essence_")) {
+			this.essenceType = name.replaceFirst("essence_", "");
+		} else {
+			this.essenceType = name;
+		}
+
+		OreDictionary.registerOre("itemEssence", this);
+		OreDictionary.registerOre("essence" + WordUtils.capitalizeFully(this.essenceType), this);
+
 	}
 
 }
--- src/main/java/com/icemetalpunk/totemessentials/items/totems/ItemTotemBase.java
+++ src/main/java/com/icemetalpunk/totemessentials/items/totems/ItemTotemBase.java
@@ -1,11 +1,25 @@
 package com.icemetalpunk.totemessentials.items.totems;
 
+import org.apache.commons.lang3.text.WordUtils;
+
 import com.icemetalpunk.totemessentials.items.TEItem;
+
+import net.minecraftforge.oredict.OreDictionary;
 
 public class ItemTotemBase extends TEItem {
 
+	public final String totemType;
+
 	public ItemTotemBase(String name) {
 		super(name);
+		if (name.endsWith("_totem")) {
+			this.totemType = name.replaceAll("_totem$", "");
+		} else {
+			this.totemType = name;
+		}
+
+		OreDictionary.registerOre("itemTotem", this);
+		OreDictionary.registerOre("totem" + WordUtils.capitalizeFully(this.totemType), this);
 	}
 
 }
