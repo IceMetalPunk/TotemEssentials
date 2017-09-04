6796a10b5ac8d0448af7dce2843bdddac94c8dd8
IceMetalPunk <icemetalpunk@gmail.com>
2017-09-03 23:25:12 -0400

    Added @Instance of mod for future use

--- src/main/java/com/icemetalpunk/totemessentials/TotemEssentials.java
+++ src/main/java/com/icemetalpunk/totemessentials/TotemEssentials.java
@@ -9,6 +9,7 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
@@ -19,6 +20,9 @@ public class TotemEssentials {
 	public static final String MODID = "totemessentials";
 	public static final String VERSION = "0.1";
 
+	 @Instance
+	 public static TotemEssentials instance = new TotemEssentials();
+	
 	@SidedProxy(clientSide = "com.icemetalpunk.totemessentials.proxies.TEClientProxy", serverSide = "com.icemetalpunk.totemessentials.proxies.TEServerProxy")
 	public static TECommonProxy proxy;
 
